3d6dcfe3230c1a606e7e114b4df2693158b94628
CodeConscious <hello@codeconscio.us>
2022-03-10 22:54:46 +0900

    Parse JSON to records

--- weather-cli/Program.cs
+++ weather-cli/Program.cs
@@ -1,36 +1,40 @@
 ﻿using System;
 using System.IO;
 using System.Net.Http;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Spectre.Console;
 
-namespace WeatherCLI
+namespace WeatherCLI;
+
+internal static class Program
 {
-    internal class Program
+    private static HttpClient _client { get; set; } = new();
+    private const string _keyFile = "openweathermap.apikey";
+    private static string _apiKey { get; set; }
+    private const string _units = "metric";
+
+    static async Task Main(string[] args)
     {
-        private static HttpClient _client { get; set; } = new();
-        private const string _keyFile = "openweathermap.apikey";
-        private static string _apiKey { get; set; }
-
-        static async Task Main(string[] args)
+        if (!File.Exists(_keyFile))
         {
-            if (!File.Exists(_keyFile))
-            {
-                AnsiConsole.WriteLine($"Cannot find \"{_keyFile}\", so aborting.");
-                return;
-            }
-
-            AnsiConsole.WriteLine($"Reading API key from \"{_keyFile}\"");
-            _apiKey = File.ReadAllText(_keyFile);
-            AnsiConsole.WriteLine("API key retrieved");
-
-            const string lat = "35.1815";
-            const string lon = "136.9066";
-
-            var result = await _client.GetStringAsync($"https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid={_apiKey}");
-
-            AnsiConsole.WriteLine("JSON data retrieved:");
-            AnsiConsole.WriteLine(result);
+            AnsiConsole.WriteLine($"Cannot find \"{_keyFile}\", so aborting.");
+            return;
         }
+
+        AnsiConsole.WriteLine($"Reading API key from \"{_keyFile}\"");
+        _apiKey = File.ReadAllText(_keyFile);
+        AnsiConsole.WriteLine("API key retrieved");
+
+        const string lat = "35.1815";
+        const string lon = "136.9066";
+
+        var result = await _client.GetStringAsync($"https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&units={_units}&appid={_apiKey}");
+
+        AnsiConsole.WriteLine("JSON data retrieved:");
+        // File.WriteAllText("forecast.json", result);
+
+        var forecast = JsonSerializer.Deserialize<Forecast.Root>(result);
+        AnsiConsole.WriteLine($"The current temperature is {forecast.Current.Temp} degrees, but it feels like {forecast.Current.FeelsLike} degrees.");
     }
 }
