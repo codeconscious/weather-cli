5f9cc13fb55a87ac5e1b8a56b905d2aa505186d0
CodeConscious <hello@codeconscio.us>
2023-07-17 10:00:17 +0900

    Rename method

--- weather-cli/Options.cs
+++ weather-cli/Options.cs
@@ -12,16 +12,15 @@ public sealed record class Options
 
     public Options(string latitude, string longitude)
     {
-
-        this.Latitude = IsStringAnAllowableNumeric(latitude, -90, 90)
+        this.Latitude = IsStringNumericWithinRange(latitude, -90, 90)
             ? latitude
             : throw new ArgumentException($"An invalid latitude ({latitude}) was provided.");
 
-        this.Longitude = IsStringAnAllowableNumeric(longitude, -180, 180)
+        this.Longitude = IsStringNumericWithinRange(longitude, -180, 180)
             ? longitude
             : throw new ArgumentException($"An invalid longitude ({longitude}) was provided.");
 
-        static bool IsStringAnAllowableNumeric(string text, float minAllowed, float maxAllowed)
+        static bool IsStringNumericWithinRange(string text, float minAllowed, float maxAllowed)
         {
             return !string.IsNullOrWhiteSpace(text) &&
                    float.TryParse(text, out var textAsFloat) &&
